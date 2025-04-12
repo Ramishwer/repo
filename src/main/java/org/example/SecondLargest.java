15a9a2e1f8d1ba85713b3f2bd4889c13b99b5258
HP <ramishwer3121999@gmail.com>
2025-04-12 16:14:55 +0530

    second

---
+++ src/main/java/org/example/SecondLargest.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class SecondLargest {
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 20, 8, 15};
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > first) {
+                second = first;
+                first = num;
+            } else if (num > second && num != first) {
+                second = num;
+            }
+        }
+
+        System.out.println("Second largest: " + second);
+    }
+}
+
